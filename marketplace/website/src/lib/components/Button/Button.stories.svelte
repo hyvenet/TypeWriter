7c61267186753b08006d72ad9c47767e80905904
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2025-01-05 10:38:18 +0100

    [Marketplace] Added button(Marten tryout)

---
+++ marketplace/website/src/lib/components/Button/Button.stories.svelte
@@ -0,0 +1,25 @@
+<script module>
+    import { defineMeta } from '@storybook/addon-svelte-csf';
+
+    import Button from './Button.svelte';
+
+    const { Story } = defineMeta({
+        component: Button,
+        tags: ['autodocs'],
+        argTypes: {
+            text: {
+                control: { type: 'text' },
+            },
+            variant: {
+                options: ['primary', 'secondary', 'accent', 'danger'],
+                control: { type: 'select' },
+            },
+        },
+        args: {
+            text: 'Click me',
+            variant: 'primary',
+        },
+    });
+</script>
+
+<Story name="Default" />
