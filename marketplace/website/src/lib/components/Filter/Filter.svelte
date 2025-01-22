844e6523c842e19dde08d99e983870a237c68217
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2025-01-22 20:56:42 +0100

    [Marketplace] Added filter component

---
+++ marketplace/website/src/lib/components/Filter/Filter.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+import {Checkbox} from "$components/ui/checkbox";
+import {Label} from "$components/ui/label";
+import FilterContainer from "$components/FilterContainer/FilterContainer.svelte";
+</script>
+
+<aside class="text-white">
+    <div class="flex items-center justify-between">
+        <h2 class="text-lg font-semibold">Filter</h2>
+        <button class="text-sm text-gray-500">Clear</button>
+    </div>
+    <div class="mt-4">
+        <FilterContainer name="Game Version">
+            <Checkbox /><Label>1.20.6</Label>
+            <Checkbox /><Label>1.20.5</Label>
+        </FilterContainer>
+    </div>
+</aside>
---
+++ marketplace/website/src/lib/components/FilterContainer/FilterContainer.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import type { HTMLAttributes } from "svelte/elements";
+    import type { Snippet } from "svelte";
+
+    interface Props extends HTMLAttributes<HTMLDivElement> {
+        children?: Snippet;
+        name: string;
+    }
+    let { name = "", children }: Props = $props();
+
+</script>
+
+<div>
+    <h3 class="text-sm font-semibold">{name}</h3>
+    <div class="mt-2 space-y-2">
+        {@render children?.()}
+    </div>
+</div>
