c5502cdb80de0dce691081a1513a59e79bc65393
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2025-01-31 17:22:35 +0100

    [Marketplace] Created entry component
    Kerz wanted me to make this

---
+++ marketplace/website/src/lib/components/Entries/Entry.stories.svelte
@@ -0,0 +1,17 @@
+<script module>
+    import { defineMeta } from "@storybook/addon-svelte-csf";
+    import Entry from "./Entry.svelte";
+
+    const { Story } = defineMeta({
+        component: Entry,
+        tags: ["autodocs"],
+        args: {
+            color: "bg-blue-500",
+            icon: "ci:shuffle",
+            title: "Add Ambient Sound Activity",
+            description: "Play an ambient sound for an entity"
+        }
+    });
+</script>
+
+<Story name="Default" />
---
+++ marketplace/website/src/lib/components/Entries/Entry.svelte
@@ -0,0 +1,24 @@
+<script>
+	import Icon from "@iconify/svelte";
+	export let color = "";
+	export let icon = "";
+	export let title = "";
+	export let description = "";
+</script>
+
+<div
+	class={`mt-2 flex w-96 cursor-pointer items-center rounded-lg bg-[#171719] text-lg text-[#e6e0e9]`}
+>
+	<div class="mr-2 flex items-center justify-center rounded p-4 {color}">
+		<Icon {icon} color="#171719" className={`text-2xl`} />
+	</div>
+	<div class="flex-grow">
+		<div class={`select-none text-[16px] font-medium`}>
+			Add {title}
+		</div>
+		<div class="select-none text-sm text-[#e6e0e9]">
+			{description}
+		</div>
+	</div>
+	<span class={`select-none pr-5 text-2xl`}>+</span>
+</div>
